300a1250bea3f9c9fb46f9ac18aee079a3a4ca0a
Sergiy <sergiy.r.melnik@gmail.com>
2025-02-23 21:19:54 +0300

    added event handlers and GUI elements

--- ClientForm.Designer.cs
+++ ClientForm.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Lab1
+﻿using System.Windows.Forms;
+
+namespace Lab1
 {
     partial class ClientForm
     {
@@ -28,13 +30,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.SuspendLayout();
+            // 
+            // ClientForm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "ClientForm";
+            this.Tag = "111";
+            this.Load += new System.EventHandler(this.ClientForm_Load);
+            this.ResumeLayout(false);
+            this.Text = "Управление клиентами";
+            this.Width = 500;
+            this.Height = 400;
+            //
+            this.nameTextBox = new TextBox();
+            this.nameTextBox.Location = new System.Drawing.Point(10, 10);
+            this.nameTextBox.Width = 150;
+            this.nameTextBox.Text = "Имя";
+            //
+            this.emailTextBox = new TextBox();
+            this.emailTextBox.Location = new System.Drawing.Point(170, 10);
+            this.emailTextBox.Width = 150;
+            this.emailTextBox.Text = "Email";
+            //
+            this.phoneTextBox = new TextBox();
+            this.phoneTextBox.Location = new System.Drawing.Point(330, 10);
+            this.phoneTextBox.Width = 100;
+            this.phoneTextBox.Text = "Телефон";
+            //
+            this.addressTextBox = new TextBox();
+            this.addressTextBox.Location = new System.Drawing.Point(10, 40);
+            this.addressTextBox.Width = 450;
+            this.addressTextBox.Multiline = true;
+            this.addressTextBox.Text = "Адрес";
+            //
+            this.addClientButton = new Button();
+            this.addClientButton.Location = new System.Drawing.Point(10, 80);
+            this.addClientButton.Text = "Добавить";
+            this.addClientButton.Width = 100;
+            this.addClientButton.Click += AddClientButton_Click;
+            //
+            this.removeClientButton = new Button();
+            this.removeClientButton.Location = new System.Drawing.Point(120, 80);
+            this.removeClientButton.Text = "Удалить";
+            this.removeClientButton.Width = 100;
+            this.removeClientButton.Click += RemoveClientButton_Click;
+            //
+            this.searchTextBox = new TextBox();
+            this.searchTextBox.Location = new System.Drawing.Point(10, 110);
+            this.searchTextBox.Width = 200;
+            this.searchTextBox.Text = "Поиск";
+            //
+            this.searchButton = new Button();
+            this.searchButton.Location = new System.Drawing.Point(220, 110);
+            this.searchButton.Text = "Искать";
+            this.searchButton.Width = 80;
+            this.searchButton.Click += SearchButton_Click;
+            //
+            this.clientsListBox = new ListBox();
+            this.clientsListBox.Location = new System.Drawing.Point(10, 140);
+            this.clientsListBox.Width = 450;
+            this.clientsListBox.Height = 200;
+            //
+            this.Controls.Add(nameTextBox);
+            this.Controls.Add(emailTextBox);
+            this.Controls.Add(phoneTextBox);
+            this.Controls.Add(addressTextBox);
+            this.Controls.Add(addClientButton);
+            this.Controls.Add(removeClientButton);
+            this.Controls.Add(searchTextBox);
+            this.Controls.Add(searchButton);
+            this.Controls.Add(clientsListBox);
+
         }
 
         #endregion
+        private TextBox nameTextBox;
+        private TextBox emailTextBox;
+        private TextBox phoneTextBox;
+        private TextBox addressTextBox;
+        private Button addClientButton;
+        private Button removeClientButton;
+        private TextBox searchTextBox;
+        private Button searchButton;
+        private ListBox clientsListBox;
     }
 }
 
--- ClientForm.cs
+++ ClientForm.cs
@@ -13,9 +13,95 @@ namespace Lab1
 {
     public partial class ClientForm : Form
     {
+        private ClientManager clientManager;
         public ClientForm()
         {
+
+            clientManager = new ClientManager();
             InitializeComponent();
+
+        }
+
+        private void ClientForm_Load(object sender, EventArgs e)
+        {
+            UpdateClientsList();
+        }
+        private void UpdateClientsList()
+        {
+            clientsListBox.Items.Clear();
+            foreach (var client in clientManager.Clients)
+            {
+                clientsListBox.Items.Add($"{client.Name} - {client.Email} ({client.Phone})");
+            }
+        }
+
+        private void AddClientButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(nameTextBox.Text) || string.IsNullOrEmpty(emailTextBox.Text)
+            || string.IsNullOrEmpty(phoneTextBox.Text) || string.IsNullOrEmpty(addressTextBox.Text))
+            {
+                MessageBox.Show("Заполните все поля!");
+                return;
+            }
+            Client newClient = new Client(nameTextBox.Text, emailTextBox.Text,
+            phoneTextBox.Text, addressTextBox.Text);
+            try
+            {
+                clientManager.AddClient(newClient);
+                nameTextBox.Clear();
+                emailTextBox.Clear();
+                phoneTextBox.Clear();
+                addressTextBox.Clear();
+                UpdateClientsList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void RemoveClientButton_Click(object sender, EventArgs e)
+        {
+            if (clientsListBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Выберите клиента для удаления!");
+                return;
+            }
+            string selectedItem = clientsListBox.SelectedItem.ToString();
+            string[] parts = selectedItem.Split(new[] { '-' }, StringSplitOptions.None);
+            if (parts.Length >= 2)
+            {
+                string name = parts[0].Trim();
+                string email = parts[1].Trim();
+                /*               email = email.Remove(email.IndexOf(" "));*/
+                var clientToRemove = clientManager.Clients.Find(c => c.Name == name && c.Email + " (" + c.Phone + ")"
+                == email);
+                if (clientToRemove != null)
+                {
+                    try
+                    {
+                        clientManager.RemoveClient(clientToRemove);
+                        UpdateClientsList();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(searchTextBox.Text))
+            {
+                UpdateClientsList();
+                return;
+            }
+            var searchResults = clientManager.SearchClients(searchTextBox.Text);
+            clientsListBox.Items.Clear();
+            foreach (var client in searchResults)
+            {
+                clientsListBox.Items.Add($"{client.Name} - {client.Email} ({client.Phone})");
+            }
         }
     }
 }
